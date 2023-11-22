af8afe168f0339048a904402403816d82a8b92b3
pedroforadori <foradorif@gmail.com>
2023-11-22 16:31:58 -0300

    feat: final project 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Correios.Demo;
 
-app.MapGet("/", () => "API ON");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+var app = builder.Build();
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+
+var itens = new List<Item>()
+{
+    new()
+    {
+        Nome = "Produto A",
+        Altura = 2,
+        Largura = 22,
+        Comprimento = 32,
+        Peso = (decimal?)0.3
+    },
+    new()
+    {
+        Nome = "Produto B"
+    }
+};
+
+List<string> listPrecoPrazo = new List<string>();
+
+app.MapGet("/health", () => "API ON");
+
+app.MapPost("/price", (string cepOrigem, string cepDestino) =>
+{
+    var precoPrazoList = new CorreiosManager().CalcularPrecoPrazo(cepOrigem, cepDestino, itens);
+
+    foreach (var precoPrazo in precoPrazoList)
+    {
+        listPrecoPrazo.Add(precoPrazo);
+    }
+
+    return listPrecoPrazo;
+});
 
 app.Run();
+
+	// "itens": {
+	// 	"Nome": "Produto A",
+	// 	"Altura": 12,
+	// 	"Largura": 14,
+	// 	"Comprimento": 16,
+	// 	"Peso": 200
+	// }  
